c510b4a572073cac62393daaae5becd25b8441e3
Water Boiled Pizza <31476555+LeptailurusServal@users.noreply.github.com>
2022-05-06 17:09:01 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='7tvAPI',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='WaterBoiledPizza',
+    author_email='',
+    description='Contact me on discord: Water Boiled Pizza#6725'
+)
